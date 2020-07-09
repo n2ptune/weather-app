34176c6169d98f8f105d2fa5c62c42258e8317b7
n2ptune <n2ptune@outlook.com>
2020-07-09 11:43:53 +0900

    feat: allow location authority to user and fetch weather data from server

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  data: () => ({
+    location: null
+  }),
 
-#nav {
-  padding: 30px;
-}
+  created() {
+    this.axios
+      .get('https://weather-server.n2ptune.vercel.app/api/weather')
+      .then(result => {
+        console.log(result.data)
+      })
+  },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  mounted() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          this.location = position
+          console.log(this.location)
+        },
+        error => {
+          console.log(error)
+        }
+      )
+    }
+  }
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
